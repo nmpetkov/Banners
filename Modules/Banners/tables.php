d8aef03842ec5a89396bc6fa70c2185b15c11c22
Michael Halbrook <michael@halbrooktech.com>
2010-05-25 17:49:39 +0000

    renaming for 1.3 standards

--- Modules/Banners/tables.php
+++ Modules/Banners/tables.php
@@ -13,77 +13,82 @@
  * This function is called internally by the core whenever the module is
  * loaded.  It adds in the information
  */
-function Banners_tables()
-{
-	// Initialise table array
-	$table = array();
+function Banners_tables() {
+    // Initialise table array
+    $table = array();
 
-	// Main banners column
-	$table['banners'] = DBUtil::getLimitedTablename('banners');
-	$table['banners_column'] = array(		'bid'       => 'bid',
-	                                       	'cid'       => 'cid',
-	                                       	'type'      => 'type',
-											'tilte'		=> 'title',
-	                                       	'imptotal'  => 'imptotal',
-	                                       	'impmade'   => 'impmade',
-	                                       	'clicks'    => 'clicks',
-	                                       	'imageurl'  => 'imageurl',
-	                                       	'clickurl'  => 'clickurl',
-	                                       	'date' => 'date');
-	$table['banners_column_def'] = array(	'bid'      => 'I AUTOINCREMENT PRIMARY',
-											'cid'		=> "I NOTNULL DEFAULT '0'",
-											'type'		=> "C(2) NOTNULL DEFAULT '0'",
-											'title'		=> "C(255) NOTNULL DEFAULT ''",
-											'imptotal'	=> "I NOTNULL DEFAULT '0'",
-											'impmade'	=> "I NOTNULL DEFAULT '0'",
-											'clicks'	=> "I NOTNULL DEFAULT '0'",
-											'imageurl'	=> "C(255) NOTNULL DEFAULT ''",
-											'clickurl'	=> "C(255) NOTNULL DEFAULT ''",
-											'date'		=> 'T DEFAULT NULL');
-	// add standard data fields
-	ObjectUtil::addStandardFieldsToTableDefinition ($table['banners_column']);
-	ObjectUtil::addStandardFieldsToTableDataDefinition($table['banners_column_def']);
+    // Main banners column
+    $table['banners'] = DBUtil::getLimitedTablename('banners');
+    $table['banners_column'] = array(
+            'bid'       => 'bid',
+            'cid'       => 'cid',
+            'type'      => 'type',
+            'tilte'     => 'title',
+            'imptotal'  => 'imptotal',
+            'impmade'   => 'impmade',
+            'clicks'    => 'clicks',
+            'imageurl'  => 'imageurl',
+            'clickurl'  => 'clickurl',
+            'date' => 'date');
+    $table['banners_column_def'] = array(
+            'bid'       => 'I AUTOINCREMENT PRIMARY',
+            'cid'       => "I NOTNULL DEFAULT '0'",
+            'type'	=> "C(2) NOTNULL DEFAULT '0'",
+            'title'	=> "C(255) NOTNULL DEFAULT ''",
+            'imptotal'	=> "I NOTNULL DEFAULT '0'",
+            'impmade'	=> "I NOTNULL DEFAULT '0'",
+            'clicks'	=> "I NOTNULL DEFAULT '0'",
+            'imageurl'	=> "C(255) NOTNULL DEFAULT ''",
+            'clickurl'	=> "C(255) NOTNULL DEFAULT ''",
+            'date'		=> 'T DEFAULT NULL');
+    // add standard data fields
+    ObjectUtil::addStandardFieldsToTableDefinition ($table['banners_column']);
+    ObjectUtil::addStandardFieldsToTableDataDefinition($table['banners_column_def']);
 
 
-	// Advertising clients
-	$table['bannersclient'] = DBUtil::getLimitedTablename('bannersclient');
-	$table['bannersclient_column'] = array(	'cid'       => 'cid',
-											'name'      => 'name',
-											'contact'   => 'contact',
-											'email'     => 'email',
-											'login'     => 'login',
-											'passwd'    => 'passwd',
-											'extrainfo' => 'extrainfo');
-	$table['bannersclient_column_def'] = array(	'cid'       => 'I AUTOINCREMENT PRIMARY',
-												'name'      => 'C(60) NOTNULL',
-												'contact'   => 'C(60) NOTNULL',
-												'email'     => 'C(60) NOTNULL',
-												'login'     => 'C(10) NOTNULL',
-												'passwd'    => 'C(10) NOTNULL',
-												'extrainfo' => "X2 NOTNULL");
-	// add standard data fields
-	ObjectUtil::addStandardFieldsToTableDefinition ($table['bannersclient_column']);
-	ObjectUtil::addStandardFieldsToTableDataDefinition($table['bannersclient_column_def']);
+    // Advertising clients
+    $table['bannersclient'] = DBUtil::getLimitedTablename('bannersclient');
+    $table['bannersclient_column'] = array(
+            'cid'       => 'cid',
+            'name'      => 'name',
+            'contact'   => 'contact',
+            'email'     => 'email',
+            'login'     => 'login',
+            'passwd'    => 'passwd',
+            'extrainfo' => 'extrainfo');
+    $table['bannersclient_column_def'] = array(
+            'cid'       => 'I AUTOINCREMENT PRIMARY',
+            'name'      => 'C(60) NOTNULL',
+            'contact'   => 'C(60) NOTNULL',
+            'email'     => 'C(60) NOTNULL',
+            'login'     => 'C(10) NOTNULL',
+            'passwd'    => 'C(10) NOTNULL',
+            'extrainfo' => "X2 NOTNULL");
+    // add standard data fields
+    ObjectUtil::addStandardFieldsToTableDefinition ($table['bannersclient_column']);
+    ObjectUtil::addStandardFieldsToTableDataDefinition($table['bannersclient_column_def']);
 
-	// completed banners
-	$table['bannersfinish'] = DBUtil::getLimitedTablename('bannersfinish');
-	$table['bannersfinish_column'] = array(	'bid'         => 'bid',
-											'cid'         => 'cid',
-											'impressions' => 'impressions',
-											'clicks'      => 'clicks',
-											'datestart'   => 'datestart',
-											'dateend'     => 'dateend');
-	$table['bannersfinish_column_def'] = array('bid'         => 'I AUTOINCREMENT PRIMARY',
-                                                 'cid'         => "I NOTNULL DEFAULT '0'",
-                                                 'impressions' => "I NOTNULL DEFAULT '0'",
-                                                 'clicks'      => "I NOTNULL DEFAULT '0'",
-                                                 'datestart'   => 'T DEFAULT NULL',
-                                                 'dateend'     => 'T DEFAULT NULL');
-	// add standard data fields
-	ObjectUtil::addStandardFieldsToTableDefinition ($table['bannersfinish_column']);
-	ObjectUtil::addStandardFieldsToTableDataDefinition($table['bannersfinish_column_def']);
+    // completed banners
+    $table['bannersfinish'] = DBUtil::getLimitedTablename('bannersfinish');
+    $table['bannersfinish_column'] = array(
+            'bid'         => 'bid',
+            'cid'         => 'cid',
+            'impressions' => 'impressions',
+            'clicks'      => 'clicks',
+            'datestart'   => 'datestart',
+            'dateend'     => 'dateend');
+    $table['bannersfinish_column_def'] = array(
+            'bid'         => 'I AUTOINCREMENT PRIMARY',
+            'cid'         => "I NOTNULL DEFAULT '0'",
+            'impressions' => "I NOTNULL DEFAULT '0'",
+            'clicks'      => "I NOTNULL DEFAULT '0'",
+            'datestart'   => 'T DEFAULT NULL',
+            'dateend'     => 'T DEFAULT NULL');
+    // add standard data fields
+    ObjectUtil::addStandardFieldsToTableDefinition ($table['bannersfinish_column']);
+    ObjectUtil::addStandardFieldsToTableDataDefinition($table['bannersfinish_column_def']);
 
-	// Return the table information
-	return $table;
+    // Return the table information
+    return $table;
 
 }
--- Modules/Banners/version.php
+++ Modules/Banners/version.php
@@ -9,15 +9,14 @@
  */
 $dom = ZLanguage::getModuleDomain('Banners');
 
-$modversion['name']           = 'Banners';
-$modversion['displayname']    = __('Banners', $dom);
-$modversion['description']    = __('Banners Management', $dom);
-$modversion['version'] = '3.0';
-$modversion['credits'] = 'pndocs/credits.txt';
-$modversion['help'] = 'pndocs/help.txt';
-$modversion['changelog'] = 'pndocs/changelog.txt';
-$modversion['license'] = 'pndocs/license.txt';
-$modversion['author'] = 'Michael Halbrook';
-$modversion['contact'] = 'http://www.halbrooktech.com';
-$modversion['securityschema'] = array('Banners::' => 'Banner ID::',
-                                      'Banners::Client' => 'Client ID::');
+$modversion['name']             = 'Banners';
+$modversion['displayname']      = __('Banners');
+$modversion['description']      = __('Banners Management');
+$modversion['version']          = '3.0';
+$modversion['credits']          = '';
+$modversion['help']             = '';
+$modversion['changelog']        = '';
+$modversion['license']          = '';
+$modversion['author']           = 'Michael Halbrook';
+$modversion['contact']          = 'http://www.halbrooktech.com';
+$modversion['securityschema']   = array('Banners::' => 'Banner ID::','Banners::Client' => 'Client ID::');
